c8994bfff8e77b6b540af2ecae73e1759df40d63
Gus <1664859gustavonajera@gmail.com>
2020-05-12 10:44:11 -0500

    temp values can vary now, and are converted

--- i2c.js
+++ i2c.js
@@ -1,17 +1,12 @@
 const i2c = require('i2c-bus');
  
 const ADDR = 0x5b;
-const TEMP_REG = 0x05;
- 
-const toCelsius = rawData => {
-  rawData = (rawData >> 8) + ((rawData & 0xff) << 8);
-  let celsius = (rawData & 0x0fff) / 16;
-  if (rawData & 0x1000) {
-    celsius -= 256;
-  }
-  return celsius;
-};
+const TEMP_REG = 0x27;
 
+const toCelsius = (t) => {
+	return (t*0.02)-273.15;
+};
+ 
 
  setInterval(()=>{
 	const i2c1 = i2c.open(1, err => {
@@ -20,7 +15,7 @@ const toCelsius = rawData => {
 	  i2c1.readWord(ADDR, TEMP_REG, (err, rawData) => {
 	    if (err) throw err;
 	 
-	    console.log(rawData);
+	    console.log({raw:rawData,C:toCelsius(rawData)});
 	 
 	    i2c1.close(err => {
 	      if (err) throw err;
